cbbd6a2e0cc7c5fc806da6d160904b4acf2c0159
tantn <tan196.tnt@gmail.com>
2021-11-07 20:57:00 +0700

    Add product moudule

--- order-service/src/app.module.ts
+++ order-service/src/app.module.ts
@@ -2,9 +2,42 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `development.env`,
+      validationSchema: Joi.object({
+        // Database configuration
+        DB_NAME: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_POST: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().allow(''),
+        DB_DATABASE: Joi.string().required(),
+    
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          port: config.get<number>('DB_POST'),
+          username: config.get<string>('DB_USERNAME'),
+          database: config.get<string>('DB_DATABASE'),
+          password: config.get<string>('DB_PASSWORD'),
+          entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+          synchronize: true,
+        };
+      },
+    }),
+    ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
