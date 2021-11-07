316e3ab31224dc1a639f52dd891fccd76bb328f7
tantn <tan196.tnt@gmail.com>
2021-11-07 23:30:02 +0700

    add payment microservice

--- payment-service/src/main.ts
+++ payment-service/src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions } from '@nestjs/microservices';
-import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { Transport, TcpOptions } from '@nestjs/microservices';
+import { PaymentModule } from './payment.module';
 
 // Create new logger instance
 const logger = new Logger('Main');
 
 async function bootstrap() {
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule
-  );
-  app.listen();
+  const app = await NestFactory.createMicroservice(PaymentModule, {
+    transport: Transport.TCP,
+    options: {
+      host: 'localhost',
+      port: 3001,
+    },
+  } as TcpOptions);
+  await app.listen();
 }
 bootstrap();
